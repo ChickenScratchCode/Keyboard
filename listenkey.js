48b1dc9408f2aa1450bcac05d614c86120826fe9
SoundMasters <43422671+SoundMasters@users.noreply.github.com>
2018-10-22 12:20:41 -0400

    Add files via upload

---
+++ listenkey.js
@@ -0,0 +1,516 @@
+alert("Javascript works");
+document.body.style.zoom="75%"
+
+
+
+    document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==49){
+        document.getElementById("11w").click();
+	
+      }
+    });
+
+	document.getElementById("11w").addEventListener("click", function(e){
+       alert("1 has been clicked2");
+      }); 
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==50){
+        document.getElementById("12w").click();
+	
+      }
+    });
+
+	document.getElementById("12w").addEventListener("click", function(e){
+       alert("2 has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==51){
+        document.getElementById("13w").click();
+	
+      }
+    });
+
+	document.getElementById("13w").addEventListener("click", function(e){
+       alert("3 has been clicked2");
+      }); 
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==52){
+        document.getElementById("14w").click();
+	
+      }
+    });
+
+	document.getElementById("14w").addEventListener("click", function(e){
+       alert("4 has been clicked2");
+      }); 
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==53){
+        document.getElementById("15w").click();
+	
+      }
+    });
+
+	document.getElementById("15w").addEventListener("click", function(e){
+       alert("5 has been clicked2");
+      }); 
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==54){
+        document.getElementById("16w").click();
+	
+      }
+    });
+
+	document.getElementById("16w").addEventListener("click", function(e){
+       alert("6 has been clicked2");
+      }); 
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==55){
+        document.getElementById("17w").click();
+	
+      }
+    });
+
+	document.getElementById("17w").addEventListener("click", function(e){
+       alert("7 has been clicked2");
+      }); 
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==56){
+        document.getElementById("18w").click();
+	
+      }
+    });
+
+	document.getElementById("18w").addEventListener("click", function(e){
+       alert("8 has been clicked2");
+      }); 
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==57){
+        document.getElementById("19w").click();
+	
+      }
+    });
+
+	document.getElementById("19w").addEventListener("click", function(e){
+       alert("9 has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==48){
+        document.getElementById("20w").click();
+	
+      }
+    });
+
+	document.getElementById("20w").addEventListener("click", function(e){
+       alert("10 has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+    document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==81){
+        document.getElementById("21w").click();
+	
+      }
+    });
+
+	document.getElementById("21w").addEventListener("click", function(e){
+       alert("q has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==87){
+        document.getElementById("22w").click();
+	
+      }
+    });
+
+	document.getElementById("22w").addEventListener("click", function(e){
+       alert("w has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){ document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==69){
+        document.getElementById("23w").click();
+	
+      }
+    });
+
+	document.getElementById("23w").addEventListener("click", function(e){
+       alert("e has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==82){
+        document.getElementById("24w").click();
+	
+      }
+    });
+
+	document.getElementById("24w").addEventListener("click", function(e){
+       alert("r has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==84){
+        document.getElementById("25w").click();
+	
+      }
+    });
+
+	document.getElementById("25w").addEventListener("click", function(e){
+       alert("t has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==89){
+        document.getElementById("26w").click();
+	
+      }
+    });
+
+	document.getElementById("26w").addEventListener("click", function(e){
+       alert("y has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==85){
+        document.getElementById("27w").click();
+	
+      }
+    });
+
+	document.getElementById("27w").addEventListener("click", function(e){
+       alert("u has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==73){
+        document.getElementById("28w").click();
+	
+      }
+    });
+
+	document.getElementById("28w").addEventListener("click", function(e){
+       alert("i has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==79){
+        document.getElementById("29w").click();
+	
+      }
+    });
+
+	document.getElementById("29w").addEventListener("click", function(e){
+       alert("o has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==80){
+        document.getElementById("30w").click();
+	
+      }
+    });
+
+	document.getElementById("30w").addEventListener("click", function(e){
+       alert("p has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+    document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==65){
+        document.getElementById("31w").click();
+	
+      }
+    });
+
+	document.getElementById("31w").addEventListener("click", function(e){
+       alert("a has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==83){
+        document.getElementById("32w").click();
+	
+      }
+    });
+
+	document.getElementById("32w").addEventListener("click", function(e){
+       alert("s has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==68){
+        document.getElementById("33w").click();
+	
+      }
+    });
+
+	document.getElementById("33w").addEventListener("click", function(e){
+       alert("d has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==70){
+        document.getElementById("34w").click();
+	
+      }
+    });
+
+	document.getElementById("34w").addEventListener("click", function(e){
+       alert("f has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){ document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==71){
+        document.getElementById("35w").click();
+	
+      }
+    });
+
+	document.getElementById("35w").addEventListener("click", function(e){
+       alert("g has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){ document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==72){
+        document.getElementById("36w").click();
+	
+      }
+    });
+
+	document.getElementById("36w").addEventListener("click", function(e){
+       alert("h has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==74){
+        document.getElementById("37w").click();
+	
+      }
+    });
+
+	document.getElementById("37w").addEventListener("click", function(e){
+       alert("j has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==75){
+        document.getElementById("38w").click();
+	
+      }
+    });
+
+	document.getElementById("38w").addEventListener("click", function(e){
+       alert("k has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==76){
+        document.getElementById("39w").click();
+	
+      }
+    });
+
+	document.getElementById("39w").addEventListener("click", function(e){
+       alert("l has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+    document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==90){
+        document.getElementById("40w").click();
+	
+      }
+    });
+
+	document.getElementById("40w").addEventListener("click", function(e){
+       alert("z has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==88){
+        document.getElementById("41w").click();
+	
+      }
+    });
+
+	document.getElementById("41w").addEventListener("click", function(e){
+       alert("x has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==67){
+        document.getElementById("42w").click();
+	
+      }
+    });
+
+	document.getElementById("42w").addEventListener("click", function(e){
+       alert("c has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){ document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==86){
+        document.getElementById("43w").click();
+	
+      }
+    });
+
+	document.getElementById("43w").addEventListener("click", function(e){
+       alert("v has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==66){
+        document.getElementById("44w").click();
+	
+      }
+    });
+
+	document.getElementById("44w").addEventListener("click", function(e){
+       alert("b has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==78){
+        document.getElementById("45w").click();
+	
+      }
+    });
+
+	document.getElementById("45w").addEventListener("click", function(e){
+       alert("n has been clicked2");
+      }); 
+/////////////////////////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", function(e){  document.getElementById
+      e.preventDefault();
+      console.log(e.keyCode);
+	  
+      if (e.keyCode==77){
+        document.getElementById("46w").click();
+	
+      }
+    });
+
+	document.getElementById("46w").addEventListener("click", function(e){
+       alert("m has been clicked2");
+      });
+/////////////////////////////////////////////////////////////////////////////////////
